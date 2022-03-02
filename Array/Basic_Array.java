89d3e05fee453e53fd6127621a65640aa84f4c1b
Hasibur Rahman <84842704+himelhrh@users.noreply.github.com>
2022-03-02 16:44:57 +0600

    Create Basic_Array.java

---
+++ Array/Basic_Array.java
@@ -0,0 +1,8 @@
+public class Basic_Array {
+    public static void main(String[] args) {
+        int [] arr = {20,30,10,50};
+        int i;
+        for(i=0; i< arr.length;i++)
+            System.out.println("The Value of Array: "+arr[i]);
+    }
+}
